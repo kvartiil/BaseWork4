21b5d72dc78172797d89d20fe1052f8aedafda04
kvartiil <138615442+kvartiil@users.noreply.github.com>
2024-06-17 13:50:43 +0300

    Initial commit

---
+++ projects/elujou-lib/src/lib/elujou-lib.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ElujouLib2Component } from '../lib/elujou-lib2/elujou-lib2.component';
+import { UustestComponent } from '../lib/elujou-uustest/elujou-uustest.component';
+
+@Component({
+  selector: 'lib-elujou-lib',
+  standalone: true,
+  templateUrl: './elujou-lib.component.html',
+  styleUrls: ['./elujou-lib.component.scss'],
+  imports: [ElujouLib2Component, UustestComponent],
+  //declarations: [ElujouLib2Component]
+
+  //imports: [],
+  //template: `
+  //  <p>
+  //    See, mis RIIALE vaja!!!!
+  //  </p>
+//  `,
+//styles: ``
+})
+export class ElujouLibComponent {
+
+muutuja = "esimene muutuja";
+
+}
